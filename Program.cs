00c2a7b94d88704e69b5d77483709642a1883603
Zedekus <51848544+Zedekus@users.noreply.github.com>
2019-06-17 06:19:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace KURS
+
+{
+
+    class Program
+    {
+
+        static void Main()
+        {
+
+            Console.Write("Введите радиус круга: ");
+        
+            string s = Console.ReadLine();
+
+            double r = Convert.ToDouble(s);
+
+            double p = Math.PI * r * r;
+
+            Console.WriteLine("Площадь круга = {0}", p);
+
+            Console.ReadLine();
+
+            return;
+
+        }
+
+    }
+
+}
